49579d33a1ec44a4bdb7c9791332e9c60b48dac3
matheus-ferreiratt <matheus.silva227@aluno.ce.gov.br>
2022-08-27 00:29:47 -0300

    modifiquei a home

---
+++ sobre/script.js
@@ -0,0 +1,12 @@
+const menuHamburguer = document.getElementById('btn-hamburguer')
+
+function ativaMenu(){
+  const navBar = document.getElementById('nav-bar')
+  navBar.classList.toggle('active')
+ 
+}
+
+menuHamburguer.addEventListener('click', ativaMenu);
+
+
+  
